323871a070828c5abcd3521f7de633a303e62f18
Sara <sara.m.adamski@gmail.com>
2021-08-04 12:45:25 -0500

    polish resume page

--- src/components/body/Resume.js
+++ src/components/body/Resume.js
@@ -1,31 +1,36 @@
 import React from 'react';
-// import Download from "../../assets/icons/download.png";
+import Download from "../../assets/icons/download.png";
 
 function Resume() {
   return (
     <div className="page-div">
-      <a href="www.google.com" className="resume-link">
-        
-        Download My Resume
-      </a>
     <h2 className="resume-header">
-      Proficiencies
+      Resume
+      <a
+        href="https://docs.google.com/document/d/19gzHWo7oFiqeqI-IxzX3EigqxJZ9sQK-hps0Lp8DCL4/export?format=pdf"
+        download
+        className="resume-link"
+      >
+        <img className="download-icon" src={Download} alt="Button to download a pdf"></img>
+      </a>
     </h2>
+    <h3 className="prof-header">Proficiencies</h3>
     <div className="page-body skill-list">
       <div className="front-list">
-        <h3 className="skill-header">Front End:</h3>
+        
+        <h4 className="skill-header">Front End</h4>
         <ul>
           <li>HTML</li>
           <li>CSS</li>
           <li>Bootstrap</li>
           <li>JavaScript</li>
           <li>jQuery</li>
-          <li>Responsive Design</li>
+          <li>Object Oriented Programming (OOP)</li>
           <li>React</li>
         </ul>
       </div>
       <div className="back-list">
-        <h3 className="skill-header">Back End:</h3>
+        <h4 className="skill-header">Back End</h4>
         <ul>
           <li>APIs</li>
           <li>Node</li>
